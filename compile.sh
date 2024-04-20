62406b2294983e43f5820c73c2f3aedd052ae601
qiusy99 <139726678+qiusy99@users.noreply.github.com>
2024-04-20 13:44:42 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-clang++ -O3 -Wall -shared -undefined dynamic_lookup -std=c++20 -fPIC $(python3 -m pybind11 --includes) csvpy.cpp -o csvpy$(python3-config --extension-suffix)
+g++ -O3 -Wall -shared -undefined dynamic_lookup -std=c++20 -fPIC $(python3 -m pybind11 --includes) csvpy.cpp -o csvpy$(python3-config --extension-suffix)
 
